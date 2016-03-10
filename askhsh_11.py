5505f9d8784def243bdda79832ef884bd3f4603d
AmyKouf <amagia@windowslive.com>
2016-03-11 01:20:12 +0200

    Added files via upload

--- askhsh_11.py
+++ askhsh_11.py
@@ -1,49 +1,39 @@
 #Askhsh 11
-#ftiaxse def an prolaveis
 import urllib2
+def source(user):
+	prefix = 'https://twitter.com/'
+	suffix = '?lang=en'
+	URL = urllib2.urlopen (prefix+user+suffix)
+	URL= URL.read()
+	URL= URL.split('<span class="ProfileNav-value" data-is-compact=')
+	for i in range (5):
+		URL [i] = URL[i].partition('</span>')[0]
+		URL [i] = URL[i].partition('>')[2]
+	return URL
+def convert(URL,i):
+	if 'K' == URL [i][-1]:
+		URL [i] = URL [i][0:-1]
+		URL [i]= float(URL [i])
+		URL [i] = URL [i] * 1000.00
+	elif 'M' == URL [i][-1]:
+		URL [i] = URL [i][0:-1]
+		URL [i]= float(URL [i])
+		URL [i] = URL [i] * 1000000.00
+	else:
+		URL [i]= float(URL [i])
+	return URL[i]
+
 user_1 = raw_input('Twitteras 1: ')
 user_2 = raw_input('Twitteras 2: ')
-prefix = 'https://twitter.com/'
-suffix = '?lang=en'
-URL_1 = urllib2.urlopen (prefix+user_1+suffix)
-URL_2 = urllib2.urlopen (prefix+user_2+suffix)
-URL_1= URL_1.read()
-URL_2= URL_2.read()
-URL_1 = URL_1.split('<span class="ProfileNav-value" data-is-compact=')
-URL_2 = URL_2.split('<span class="ProfileNav-value" data-is-compact=')
-for i in range (5):
-	URL_1 [i] = URL_1[i].partition('</span>')[0]
-	URL_2 [i] = URL_2[i].partition('</span>')[0]
-	URL_1 [i] = URL_1[i].partition('>')[2]
-	URL_2 [i] = URL_2[i].partition('>')[2]
+URL_1 = source(user_1)
+URL_2 = source(user_2)
 score_1 = 0
 score_2 = 0
-user_1 = raw_input('Finish?')
 for i in range (1,5):
 	URL_1 [i] = URL_1 [i].translate(None, ''.join(''','''))
 	URL_2 [i] = URL_2 [i].translate(None, ''.join(''','''))
-	print URL_1[i]
-	print URL_2[i]
-	if 'K' == URL_1 [i][-1]:
-		URL_1 [i] = URL_1 [i][0:-1]
-		URL_1 [i]= float(URL_1 [i])
-		URL_1 [i] = URL_1 [i] * 1000.00
-	elif 'M' == URL_1 [i][-1]:
-		URL_1 [i] = URL_1 [i][0:-1]
-		URL_1 [i]= float(URL_1 [i])
-		URL_1 [i] = URL_1 [i] * 1000000.00
-	else:
-		URL_1 [i]= float(URL_1 [i])
-	if 'K'  == URL_2 [i][-1]:
-		URL_2 [i] = URL_2 [i][0:-1]
-		URL_2 [i]= float(URL_2 [i])
-		URL_2 [i] = URL_2 [i] * 1000.00
-	elif 'M'  == URL_2 [i][-1]:
-		URL_2 [i] = URL_2 [i][0:-1]
-		URL_2 [i]= float(URL_2 [i])
-		URL_2 [i] = URL_2 [i] * 1000000.00
-	else:
-		URL_2 [i]= float(URL_2 [i])
+	URL_1 [i] = convert(URL_1,i)
+	URL_2 [i] = convert(URL_2,i)
 	if URL_1 [i]< URL_2[i]:
 		score_2 = score_2 + 1
 	elif URL_1 [i]> URL_2[i]:
--- askhsh_8.py
+++ askhsh_8.py
@@ -1,12 +1,22 @@
 #Askhsh 8
 with open("Tetris.txt") as textFile:
         lines = [line.split('\n') for line in textFile]
-for i in range (9):
-
-	print ' '.join((lines[i])
-for i in range (9):
-	print lines [8-i][0]
-print '\n'
-# j = 9-i
+sq = []
+rot = []
+for i in range (8):
+	sq.append([])
+	sq[i] = list(lines[i][0])
+for i in range (8):
+	rot.append([])
+	rot = zip(*sq[::-1])
+	print ''.join(rot[i])
+print ' '
+for i in range (8):
+	rot[i] = sq[7-i]
+	print ''.join(rot[i])
+print ' '
+for i in range (8):
+	sq = zip(*rot[::-1])
+	print ''.join(sq[i])
+print ' '
 raw_input('Finish?')
-			
